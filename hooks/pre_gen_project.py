075d6f1b8f232c1ae7cb7d288da8f8d1040f49c9
Rivo Laks <rivolaks@gmail.com>
2016-05-09 15:46:31 +0300

    Add check for cookiecutter version - at least 1.3.0 is required now

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,3 +1,18 @@
+import sys
+
+import cookiecutter
+
+
+# Ensure cookiecutter is recent enough
+cookiecutter_min_version = '1.3.0'
+if cookiecutter.__version__ < cookiecutter_min_version:
+    print("--------------------------------------------------------------")
+    print("!! Your cookiecutter is too old, at least %s is required !!" % cookiecutter_min_version)
+    print("--------------------------------------------------------------")
+    sys.exit(1)
+
+
+# Ensure the selected repo name is usable
 repo_name = '{{ cookiecutter.repo_name }}'
 assert_msg = 'Repo name should be valid Python identifier!'
 
