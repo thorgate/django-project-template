bda2cd0dae1029d126496c54c6c54beca80c50c1
Yuriy Shatrov <yks-uno@ya.ru>
2019-12-12 17:24:22 +0200

    Add some default tests
    The following django problems are addressed by the tests:
    * missing migrations (`manage.py makemigrations` should not add any new migrations)
    * configuration issues (`manage.py check` should not fail)

---
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/tests/test_django.py
@@ -0,0 +1,27 @@
+from io import StringIO
+
+from django.core.management import call_command
+from django.test import override_settings
+
+import pytest
+
+
+@pytest.mark.django_db
+def test_for_missing_migrations():
+    output = StringIO()
+    try:
+        call_command('makemigrations', interactive=False, dry_run=True, check=True, stdout=output)
+    except SystemExit:
+        pytest.fail('There are missing migrations:\n %s' % output.getvalue())
+
+
+# Without this settings patch, we get an error:
+#   (tg_utils.W001) EMAIL_HOST_PASSWORD setting is not set to proper value
+# while locally we don't need a password because we use mailhog
+@override_settings(EMAIL_HOST_PASSWORD='propervalue')
+@pytest.mark.django_db
+def test_django_checks():
+    try:
+        call_command('check', fail_level='WARNING')
+    except SystemExit:
+        pytest.fail('Django system checks failed')
