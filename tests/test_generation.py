32bc16c6f4175a9341a13addc3f7e764b430c77c
Yuriy Shatrov <yks-uno@ya.ru>
2019-12-20 18:53:16 +0200

    Make tests faster + other small improvements
    * Don't run full test suites on configurations that don't actually
      affect any tests. (I.e. the celery config doesn't have any celery-specific tests,
      so why run all the same tests without being able to prove that celery works.)
    * Remove warning on reading yaml
    * Other small fixes to the test file
    * Fix Makefile not displaying colors correctly
    * Remove the need for sudo to change Pipfile.lock ownership
    * Remove if-condition in Dockerfile-django whether to install libjpeg, because libjpeg is always installed anyway.
    * Remove one extra step of CI - installation of docker-compose outside of pipenv; add docker-compose to Pipfile
    
    Closes #8

--- tests/test_generation.py
+++ tests/test_generation.py
@@ -28,7 +28,7 @@ def validate_project_works(result, config):
     project_inner_dir = str(result.project.join(config['repo_name']))
 
     with open(os.path.join(project_dir, '.gitlab-ci.yml')) as f:
-        gitlab_ci = yaml.load(f)
+        gitlab_ci = yaml.load(f, Loader=yaml.FullLoader)
 
     # Grab commands and environment from gitlab-ci
     commands = gitlab_ci['test']['script']
@@ -63,7 +63,7 @@ def test_base_generate(cookies, default_project):
 
     assert result.project.join('.hgignore').exists()
     assert result.project.join('.gitignore').exists()
-    assert not result.project.join('%s/templates/cms_main.html' % (default_project['repo_name'],)).exists()
+    assert not result.project.join('%(repo_name)s/templates/cms_main.html' % default_project).exists()
 
     validate_project_works(result, default_project)
 
@@ -74,9 +74,12 @@ def test_cms_generate(cookies, default_project):
     })
     result = generate_project(cookies, default_project)
 
-    assert result.project.join('%s/templates/cms_main.html' % (default_project['repo_name'],)).exists()
+    assert result.project.join('%(repo_name)s/templates/cms_main.html' % default_project).exists()
 
-    validate_project_works(result, default_project)
+    # This will be accounted for in cms+celery config
+    # we don't have any cms-specific tests anyway
+    # so let's not run the full test suite to save time.
+    # validate_project_works(result, default_project)
 
 
 def test_celery_generate(cookies, default_project):
@@ -90,7 +93,10 @@ def test_celery_generate(cookies, default_project):
         contents = f.read()
     assert 'celery:' in contents
 
-    validate_project_works(result, default_project)
+    # This will be accounted for in cms+celery config
+    # we don't have any celery-specific tests anyway
+    # so let's not run the full test suite to save time.
+    # validate_project_works(result, default_project)
 
 
 def test_doc_generate(cookies, default_project):
@@ -99,10 +105,12 @@ def test_doc_generate(cookies, default_project):
     })
     result = generate_project(cookies, default_project)
 
-    assert result.project.join('%s/docs/conf.py' % (default_project['repo_name'],)).exists()
-    assert result.project.join('%s/docs/index.rst' % (default_project['repo_name'],)).exists()
+    assert result.project.join('%(repo_name)s/docs/conf.py' % default_project).exists()
+    assert result.project.join('%(repo_name)s/docs/index.rst' % default_project).exists()
 
-    validate_project_works(result, default_project)
+    # we don't have any docs-specific tests anyway
+    # so let's not run the full test suite to save time.
+    # validate_project_works(result, default_project)
 
 
 def test_doc_not_generate(cookies, default_project):
@@ -111,7 +119,7 @@ def test_doc_not_generate(cookies, default_project):
     })
     result = generate_project(cookies, default_project)
 
-    assert not result.project.join('%s/docs' % (default_project['repo_name'],)).exists()
+    assert not result.project.join('%(repo_name)s/docs' % default_project).exists()
 
 
 def test_celery_and_cms_generate(cookies, default_project):
@@ -121,7 +129,7 @@ def test_celery_and_cms_generate(cookies, default_project):
     })
     result = generate_project(cookies, default_project)
 
-    assert result.project.join('%s/templates/cms_main.html' % (default_project['repo_name'],)).exists()
+    assert result.project.join('%(repo_name)s/templates/cms_main.html' % default_project).exists()
 
     assert result.project.join('docker-compose.yml').exists()
     with open(result.project.join('docker-compose.yml')) as f:
