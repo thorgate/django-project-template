8745bea133ca1741d0cc5d637b21f5af20435c27
Manu Ganji <manu@thorgate.eu>
2020-02-17 17:03:41 +0200

    Adding a comment and if check for storybook stub

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/app/src/utils/urls.js
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/app/src/utils/urls.js
@@ -1,7 +1,10 @@
 const reverseUrl = (urlName, ...params) => {
+    {% if cookiecutter.include_storybook == "yes" %}
+    // DJ_CONST.reverse is mocked as a function in storybook
     if (typeof DJ_CONST.reverse === 'function') {
         return DJ_CONST.reverse(urlName, ...params);
     }
+    {% endif %}
     return DJ_CONST.reverse[urlName](...params);
 };
 
