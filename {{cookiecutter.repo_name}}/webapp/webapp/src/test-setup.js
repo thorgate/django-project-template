6d8bfe95ed5d01a9442460aaf8c2fa9b5668a5a2
Ragnar <ragnar@thorgate.eu>
2020-06-30 13:54:02 +0300

    Upgrade react-testing-library.

--- {{cookiecutter.repo_name}}/webapp/webapp/src/test-setup.js
+++ {{cookiecutter.repo_name}}/webapp/webapp/src/test-setup.js
@@ -1,10 +1,6 @@
 // Add some helpful assertions
 import 'jest-dom/extend-expect';
 
-// react-testing-library renders components to document.body, this will ensure
-// they're removed after each test.
-import 'react-testing-library/cleanup-after-each';
-
 // Mock global Django JavascriptCatalog methods
 /* eslint-disable no-confusing-arrow */
 global.django = {
