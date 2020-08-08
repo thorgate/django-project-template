88f454ab17a42425ce22bdeb7fd1de7fe97ab9dd
Jyrno Ader <jyrno42@gmail.com>
2020-08-08 14:15:32 +0300

    Also publish CI images to docker hub
    Only executed when $DOCKER_HUB_TOKEN and DOCKER_HUB_USER are set

--- utils/publish-ci-image.sh
+++ utils/publish-ci-image.sh
@@ -9,13 +9,15 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-IMAGE="${CI_REGISTRY_IMAGE}/ci"
+IMAGE_SUFFIX='/ci'
+IMAGE="${CI_REGISTRY_IMAGE}${IMAGE_SUFFIX}"
 IMAGE_TAG=${CI_COMMIT_REF_SLUG:=${CI_COMMIT_TAG:='master'}}
 
 echo "CI image: ${IMAGE}"
 echo "Building and tagging CI image with version ${IMAGE_TAG}"
 
-CACHE_ARG="--cache-from ${IMAGE}:master --build-arg BUILDKIT_INLINE_CACHE=1"
+CACHE_ARG="--build-arg BUILDKIT_INLINE_CACHE=1"
+CACHE_ARG="${CACHE_ARG} --cache-from ${IMAGE}:latest --cache-from ${IMAGE}:${IMAGE_TAG}"
 
 docker build $CACHE_ARG -f ${DIR}/Dockerfile-ci -t "${IMAGE}:${IMAGE_TAG}" ${DIR}
 docker build $CACHE_ARG -f ${DIR}/Dockerfile-ci -t "${IMAGE}:latest" ${DIR}
@@ -23,3 +25,19 @@ docker build $CACHE_ARG -f ${DIR}/Dockerfile-ci -t "${IMAGE}:latest" ${DIR}
 echo "Publishing CI image"
 docker push "${IMAGE}:${IMAGE_TAG}"
 docker push "${IMAGE}:latest"
+
+# DOCKER_HUB_TOKEN should be set via build secrets
+if [[ -z $DOCKER_HUB_USER ]] || [[ -z $DOCKER_HUB_TOKEN ]]; then
+    echo "DOCKER_HUB_TOKEN or DOCKER_HUB_USER env variable is not defined."
+    echo " Skipping docker hub publish"
+    env
+else
+    HUB_IMAGE="thorgate/django-template${IMAGE_SUFFIX}"
+
+    echo "Publishing ${HUB_IMAGE} to docker hub too"
+    docker tag "${IMAGE}:${IMAGE_TAG}" "${HUB_IMAGE}:${IMAGE_TAG}"
+    docker tag "${IMAGE}:latest" "${HUB_IMAGE}:latest"
+
+    echo "Authenticating against docker.io"
+    docker login -u "$DOCKER_HUB_USER" -p "$DOCKER_HUB_TOKEN" docker.io
+fi
