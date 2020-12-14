a940c974539c6892602ee6992922749df2dbf153
Yuriy Shatrov <yks-uno@ya.ru>
2020-12-14 18:15:30 +0200

    Switch to Debian-based images for the project
    * Replaced python alpine with -slim-buster.
    * Replaced node alpine with -buster-slim.
    * Replaced psycopg2 package with psycopg2-binary as had been advised in the comment.
    * Did not replace alpine in CI images so that it doesn't break CI in every project that relies on the built image.

--- extensions/tg_dockerimage_lookup.py
+++ extensions/tg_dockerimage_lookup.py
@@ -6,9 +6,8 @@ from jinja2.ext import Extension
 
 
 IMAGE_LOOKUP = {
-    '3.6': 'python:3.6-alpine3.11',
-    '3.7': 'python:3.7-alpine3.11',
-    '3.8': 'python:3.8-alpine3.11',
+    # Specific image tags that do not fall into the pattern below:
+    # '3.6': 'python:3.6-slim-buster',
 }
 
 
@@ -16,7 +15,7 @@ def python_image(value):
     try:
         return IMAGE_LOOKUP[value]
     except KeyError:
-        return f'python:{value}-alpine'
+        return f'python:{value}-slim-buster'
 
 
 class TGDockerImageModule(Extension):
