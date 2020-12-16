7519e5c0f42d2e317ae0c1332fac4643800c66ab
Yuriy Shatrov <yks-uno@ya.ru>
2020-12-16 12:19:36 +0200

    Fix poetry-check-lock.sh to return the correct exit codes

--- {{cookiecutter.repo_name}}/scripts/poetry-check-lock.sh
+++ {{cookiecutter.repo_name}}/scripts/poetry-check-lock.sh
@@ -4,7 +4,10 @@
 #  this script can be removed.
 # see https://github.com/python-poetry/poetry/issues/453
 
-set -e
-
-poetry run python -c "from poetry.factory import Factory; l = Factory().create_poetry('.').locker; exit(0) if l.is_locked() and l.is_fresh() else exit(1)" \
- && echo 'poetry.lock is up to date' || echo 'poetry.lock is out of date'
+poetry run python -c "from poetry.factory import Factory; l = Factory().create_poetry('.').locker; exit(0) if l.is_locked() and l.is_fresh() else exit(1)"
+if [ $? -eq 0 ]; then
+    echo 'poetry.lock is up to date.'
+else
+    echo '*** poetry.lock is out of date! ***' >&2
+    exit 1
+fi
