c014a21554850f1c71086a72671fb117d0739581
Yuriy Shatrov <yks-uno@ya.ru>
2021-03-06 21:44:12 +0000

    Fixed AWS S3 access denied error on new projects.
    Introduced a new boolean configuration variable `s3_media_bucket_is_public` in the top level variables.tf file.
    This var is passed down to the `s3_media` module.
    
    This variable controls some of the `aws_s3_bucket_public_access_block` settings.
    
    Set it to false to be consistent with the Django settings and the usage pattern.
    Otherwise, AWS blocks all public access to the bucket.
    
    Added a comment to PrivateMediaStorage.

--- {{cookiecutter.repo_name}}/utils/terraform/variables.tf
+++ {{cookiecutter.repo_name}}/utils/terraform/variables.tf
@@ -1,11 +1,16 @@
 // https://www.terraform.io/docs/configuration/variables.html
 
 variable "project" {
-  type    = "string"
+  type    = string
   default = "{{cookiecutter.repo_name|as_hostname}}"
 }
 
 variable "region" {
-  type    = "string"
-  default = "eu-west-1"
+  type    = string
+  default = "eu-north-1"
+}
+
+variable "s3_media_bucket_is_public" {
+  type = bool
+  default = true
 }
