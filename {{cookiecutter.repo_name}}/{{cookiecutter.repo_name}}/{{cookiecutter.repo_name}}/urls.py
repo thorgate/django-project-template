d3d7d3844790430775ac320a5c8d9016df3a26e3
Tonis Piip <tonis@thorgate.eu>
2021-08-18 17:39:48 +0300

    Fix urls

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/urls.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/urls.py
@@ -11,16 +11,15 @@ from django.views.generic.base import RedirectView{% endif %}
 admin.autodiscover()
 
 urlpatterns = [
-{%- if cookiecutter.frontend_style == 'webapp' %}
+    {%- if cookiecutter.frontend_style == 'webapp' %}
     path("", include("accounts.urls")),
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
     path("jsi18n/", JavaScriptCatalog.as_view(), name="javascript-catalog"),{% else %}
     path("api/", include("{{cookiecutter.repo_name}}.rest.urls")),{% endif %}
     path("{{cookiecutter.django_admin_path}}/", admin.site.urls),
+    *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
+    *static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if not settings.DEBUG:
     handler500 = "{{cookiecutter.repo_name}}.views.server_error"
