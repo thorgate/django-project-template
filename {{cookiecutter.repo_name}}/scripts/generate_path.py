71e7d1e89bce9449938e3071a6161327386cb5c3
Jyrno Ader <jyrno@thorgate.eu>
2021-08-31 11:57:05 +0300

    Enable cobertura test reporting and inline coverage for SPA and django
    - SPA Demo:
      - see https://gitlab.com/thorgate/confident_curie/-/merge_requests/1/diffs
    - Webapp Demo:
      - see https://gitlab.com/thorgate/eager_cannon/-/merge_requests/1/diffs
    
    Also:
    
    - Ensure pyproject.toml symlink is always generated
      - update the default ENV_FOLDER to account for this
    - pass `cmd` env var contents to:
      - coverage-py
      - coverage-js
      - test-node
      - test-djagno
    - Fix poetry author format to avoid an author regex error

--- {{cookiecutter.repo_name}}/scripts/generate_path.py
+++ {{cookiecutter.repo_name}}/scripts/generate_path.py
@@ -4,7 +4,7 @@ from poetry.utils.env import EnvManager
 
 
 if __name__ == "__main__":
-    # / since pyproject.toml is copied to the root folder
+    # /app since pyproject.toml in the foot folder is symlinked from internal folder
     env_name = EnvManager.generate_env_name(
         "{{ cookiecutter.repo_name | as_hostname }}", "/"
     )
