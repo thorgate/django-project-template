5deea13c326caef26aafc592bf82e16adb1ed83b
Jorgen Ader <jorgen.ader@gmail.com>
2022-03-10 12:39:16 +0200

    DEVOPS-38: Added safeguards for mirror-prepare script

--- {{cookiecutter.repo_name}}/ansible/mirror-prepare.sh
+++ {{cookiecutter.repo_name}}/ansible/mirror-prepare.sh
@@ -17,31 +17,40 @@ then
     exit 1
 fi
 
+yq --help | grep 'https://github.com/kislyuk/yq' &> /dev/null || (echo "Error: Not using correct yq" && exit 1)
+
 echo "Turning off the site"
 docker-compose down
 
 echo "Detecting media dir"
-the_dir=`docker-compose config | yq -r '.services.django.volumes[]' | grep '/files/media' | cut -d: -f1`
-echo "Media dir is ${the_dir}"
+media_dir=`docker-compose config | yq -r '.services.django.volumes[]' | grep '/files/media' | cut -d: -f1`
+data_dir=`dirname ${media_dir}`
+
+if [[ "$data_dir" != *".data" ]]; then
+    echo "Data directory does not end with .data"
+    exit 1
+fi
+
+echo "Media dir is ${media_dir}"
 
 echo "Changing .data dir ownership to ${USER}"
-sudo chown -R ${USER}: ${the_dir}/..
+sudo chown -R ${USER}: ${data_dir}
 
 echo "Changing media dir ownership to ${USER}"
-sudo chown -R ${USER}: ${the_dir}
+sudo chown -R ${USER}: ${media_dir}
 
 echo "Detecting postgres data dir"
-the_dir=`docker-compose config | yq -r '.services.postgres.volumes[]' | grep '/var/lib/postgresql/data' | cut -d: -f1`
-echo "Postgres dir is ${the_dir}"
+postgres_dir=`docker-compose config | yq -r '.services.postgres.volumes[]' | grep '/var/lib/postgresql/data' | cut -d: -f1`
+echo echo "Postgres dir is ${postgres_dir}"
 
 echo "Changing postgres dir ownership to ${USER}"
-sudo chown -R ${USER}: ${the_dir}
+sudo chown -R ${USER}: ${postgres_dir}
 
 echo "Detecting postgres mirror dir"
-the_dir=`docker-compose config | yq -r '.services.postgres.volumes[]' | grep 'db-mirror' | cut -d: -f1`
-echo "Postgres mirror dir is ${the_dir}"
+postgres_dir=`docker-compose config | yq -r '.services.postgres.volumes[]' | grep 'db-mirror' | cut -d: -f1`
+echo "Postgres mirror dir is ${postgres_dir}"
 
-sudo mkdir -p ${the_dir}
-sudo chown -R ${USER}: ${the_dir}
+sudo mkdir -p ${postgres_dir}
+sudo chown -R ${USER}: ${postgres_dir}
 
 echo "DONE"
