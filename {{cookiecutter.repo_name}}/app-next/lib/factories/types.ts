7e61bf59eea65560d8bee619b4f435c973a522bd
Sergey Kovalskiy <sergey@thorgate.eu>
2024-06-05 13:23:44 +0300

    fix: results type

--- {{cookiecutter.repo_name}}/app-next/lib/factories/types.ts
+++ {{cookiecutter.repo_name}}/app-next/lib/factories/types.ts
@@ -29,7 +29,7 @@ export interface RetrieveQueryPageOnlyResult {
 }
 export interface RetrieveQueryResult<ItemType>
     extends RetrieveQueryPageOnlyResult {
-    results?: ItemType[];
+    results: ItemType[];
 }
 
 export type BaseItemType = object;
